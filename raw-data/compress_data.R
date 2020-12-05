ec6a3269a10142623385b8d53efe3301fd7e1750
GL-Li <liguanglai@gmail.com>
2020-12-05 09:43:42 -0500

    compress dataset

---
+++ raw-data/compress_data.R
@@ -0,0 +1,23 @@
+save(apartment_apps, file = "data/apartment_apps.RData", compress = "xz", compression_level = 9)
+
+save(auto_claim, file = "data/auto_claim.RData", compress = "xz", compression_level = 9)
+
+save(bank_loans, file = "data/bank_loans.RData", compress = "xz", compression_level = 9)
+
+save(boston, file = "data/boston.RData", compress = "xz", compression_level = 9)
+
+save(customer_phone_calls, file = "data/customer_phone_calls.RData", compress = "xz", compression_level = 9)
+
+save(customer_value, file = "data/customer_value.RData", compress = "xz", compression_level = 9)
+
+save(exam_pa_titanic, file = "data/exam_pa_titanic.RData", compress = "xz", compression_level = 9)
+
+save(health_insurance, file = "data/health_insurance.RData", compress = "xz", compression_level = 9)
+
+save(june_pa, file = "data/june_pa.RData", compress = "xz", compression_level = 9)
+
+save(patient_length_of_stay, file = "data/patient_length_of_stay.RData", compress = "xz", compression_level = 9)
+
+save(patient_num_labs, file = "data/patient_num_labs.RData", compress = "xz", compression_level = 9)
+
+save(readmission, file = "data/readmission.RData", compress = "xz", compression_level = 9)
