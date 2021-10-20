794d7a0b663e448afe04c459df2c0357704943a1
GL-Li <liguanglai@gmail.com>
2021-10-20 19:05:07 -0400

    v0.5.0

--- R/travel_spending.R
+++ R/travel_spending.R
@@ -13,7 +13,7 @@
 #'   \item{Distance}{Distance traveled in trip, in km}
 #'   \item{Duration}{Number of nights spent on trip}
 #'   \item{Reason}{Main reason for the trip. Vacation includes holiday, leisure, or recration. Visit includes visiting friends or relatives.}
-#'   \item{Age}{Age of adult survey respondent}
+#'   \item{Age}{Age of adult survey respondent in six age bins}
 #'   \item{Gender}{Gender of adult survey respondent}
 #'   \item{HHI}{Household income, in Canadian $}
 #'   \item{Others}{Number of other persons that accompanied the respondent on the trip}
